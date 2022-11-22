6092b3fd903adc76ebbe365bb9cd95026e8d5467
Fauzan Akmal Mahdi <recovery252@gmail.com>
2022-11-22 17:57:26 +0700

    update belajar golang

--- main.go
+++ main.go
@@ -9,7 +9,78 @@ func main() {
 	// arrayTest()
 	// arrayTest2()
 	// multidimensiArray()
-	loopingArray()
+	// loopingArray()
+	// sliceArray()
+	// sliceArray2()
+	// belajarMap()
+	// belajarMap2()
+	loopingMap()
+}
+
+func loopingMap() {
+	// cara horizontal
+	var chicken1 = map[string]int{"januari": 50, "februari": 40, "maret": 35, "april": 70}
+	for key, value := range chicken1 {
+		fmt.Println(key, "  \t", value)
+	}
+}
+
+func belajarMap2() {
+	// cara horizontal
+	var chicken1 = map[string]int{"januari": 50, "februari": 40}
+
+	// cara vertical
+	var chicken2 = map[string]int{
+		"januari":  50,
+		"februari": 40,
+	}
+
+	fmt.Println(chicken1)
+	fmt.Println(chicken2)
+}
+
+func belajarMap() {
+	// var chicken map[string]int
+	chicken := map[string]int{}
+
+	chicken["januari"] = 50
+	chicken["februari"] = 40
+
+	fmt.Println("januari", chicken["januari"]) // januari 50
+	fmt.Println("mei", chicken["mei"])         // mei 0
+}
+
+func sliceArray2() {
+	var fruits = []string{"apple", "grape", "banana", "melon"}
+
+	var aFruits = fruits[0:3]
+	var bFruits = fruits[1:4]
+
+	var aaFruits = aFruits[1:2]
+	var baFruits = bFruits[0:1]
+
+	fmt.Println(fruits)   // [apple grape banana melon]
+	fmt.Println(aFruits)  // [apple grape banana]
+	fmt.Println(bFruits)  // [grape banana melon]
+	fmt.Println(aaFruits) // [grape]
+	fmt.Println(baFruits) // [grape]
+
+	// Buah "grape" diubah menjadi "pinnaple"
+	baFruits[0] = "pinnaple"
+
+	fmt.Println(fruits)   // [apple pinnaple banana melon]
+	fmt.Println(aFruits)  // [apple pinnaple banana]
+	fmt.Println(bFruits)  // [pinnaple banana melon]
+	fmt.Println(aaFruits) // [pinnaple]
+	fmt.Println(baFruits) // [pinnaple]
+}
+
+func sliceArray() {
+	// array1 := [...][3]string{{"fauzan", "akmal", "mahdi"}, {"lagi", "belajar golang"}}
+	array1 := [...]string{"fauzan", "akmal", "mahdi", "lagi", "belajar golang"}
+
+	fmt.Println("panjang array\t:", len(array1))
+	fmt.Println("isi array\t:", array1[0:3])
 }
 
 func loopingArray() {
